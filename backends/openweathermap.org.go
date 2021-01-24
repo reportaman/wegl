7a4eb81b27902966d5af3809ff90434a3ff330c1
reportaman <71736629+reportaman@users.noreply.github.com>
2021-01-23 18:11:08 -0800

    Update openweathermap.org.go

--- backends/openweathermap.org.go
+++ backends/openweathermap.org.go
@@ -247,7 +247,6 @@ func (c *openWeatherConfig) Fetch(location string, numdays int) iface.Data {
 	}
 	ret.Current, err = c.parseCond(resp.List[0])
 	ret.Location = fmt.Sprintf("%s, %s", resp.City.Name, resp.City.Country)
-//	fmt.Println("Hello world")
 	fmt.Println(ret.Current)
 	if err != nil {
 		log.Fatalf("Failed to fetch weather data: %v\n", err)
